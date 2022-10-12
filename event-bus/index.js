68a83ace99472c211f81a324165aa5c3c2a16978
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-10-12 21:59:20 +0500

    event syncing in action

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,15 +5,23 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {console.log(err.message)});
-    axios.post('http://localhost:4001/events', event).catch((err) => {console.log(err.message)});
-    axios.post('http://localhost:4002/events', event).catch((err) => {console.log(err.message)});
-    axios.post('http://localhost:4003/events', event).catch((err) => {console.log(err.message)});
+    events.push(event);
+
+    axios.post('http://localhost:4000/events', event).catch((err) => {console.log(err.message, 1)});
+    axios.post('http://localhost:4001/events', event).catch((err) => {console.log(err.message, 2)});
+    axios.post('http://localhost:4002/events', event).catch((err) => {console.log(err.message, 3)});
+    axios.post('http://localhost:4003/events', event).catch((err) => {console.log(err.message, 4)});
 
     res.send({status: 'OK'});
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
